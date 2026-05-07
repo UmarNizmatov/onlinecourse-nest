2e6ba10fe56b7d35301640bee43d833157a0d223
Umar Nizmatov <anos456709@gmail.com>
2026-05-07 20:43:41 +0500

    courses start

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Auth } from './auth/entities/auth.entity';
 import { ConfigModule } from '@nestjs/config';
+import { CoursesModule } from './courses/courses.module';
 
 @Module({
   imports: [
@@ -20,6 +21,7 @@ import { ConfigModule } from '@nestjs/config';
       autoLoadEntities: true,
       synchronize: true,
     }),
+    CoursesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
